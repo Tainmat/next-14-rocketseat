69301098185dd6a4709cae4ddfb148ebc9213204
Tainã Pino <tmartins.pino@gmail.com>
2024-03-27 08:00:10 -0300

    feat: add abtraction client components

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,3 +1,6 @@
-export default function Catalog() {
+export default async function Catalog() {
+  // eslint-disable-next-line prettier/prettier
+  await new Promise(resolve => setTimeout(resolve, 2000))
+
   return <h1>Hello Catalogo</h1>
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
 export default async function Home() {
+  // eslint-disable-next-line prettier/prettier
+  await new Promise(resolve => setTimeout(resolve, 2000))
+
   const response = await fetch('https://api.github.com/users/Tainmat')
   const user = await response.json()
 
